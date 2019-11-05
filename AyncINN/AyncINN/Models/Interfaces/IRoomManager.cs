5c29e5b08df3e50819f74b27b21de5da045e2d40
Biniam <biniamt23@yahoo.com>
2019-11-05 13:10:11 -0800

    interface and service for rooms completed and working

---
+++ AyncINN/AyncINN/Models/Interfaces/IRoomManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AyncINN.Models.Interfaces
+{
+    public interface IRoomManager
+    {
+        //Create hotel
+            Task CreateRoom(Room room);
+
+            //Get individual Hotel
+            Task<Room> GetRoomByID(int id);
+
+            //Get all hotels
+            Task<List<Room>> GetRooms();
+
+            //Update hotel
+            Task UpdateRoom(Room room);
+
+            //Delete hotel
+            Task DeleteRoom(int id);
+
+    }
+}
---
+++ AyncINN/AyncINN/Models/Services/RoomService.cs
@@ -0,0 +1,48 @@
+﻿using AyncINN.Data;
+using AyncINN.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AyncINN.Models.Services
+{
+    public class RoomService : IRoomManager
+    {
+        private AsyncInnDbContext _context;
+
+        public RoomService(AsyncInnDbContext context)
+        {
+            _context = context;
+
+        }
+        public async Task CreateRoom(Room room)
+        {
+            await _context.Room.AddAsync(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteRoom(int id)
+        {
+            Room room = await GetRoomByID(id);
+            _context.Room.Remove(room);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Room> GetRoomByID(int id) => await _context.Room.FirstOrDefaultAsync(rm => rm.ID == id);
+
+
+        public async Task<List<Room>> GetRooms()
+        {
+            List<Room> rooms = await _context.Room.ToListAsync();
+            return rooms;
+        }
+
+        public async Task UpdateRoom(Room room)
+        {
+            _context.Room.Update(room);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
